e4ab675c6b207c7cb25742fe3cb30cb4c28842ef
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-02-12 21:29:15 -0500

    wildcard big fix

--- Develop/server.js
+++ Develop/server.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const path = require('path');
+const notes = require('./db/db.json')
 
+// setting port
 const PORT = process.env.PORT || 3001;
 
+// 
 const app = express();
 
+// get request to return indes.html
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// get request/ wild card route that return index.html
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
 
+// get request that return notes.html at the /notes path
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
+
+// get request that return notes from db
+app.get('/api/notes', (req,res) => {
+    res.json(notes);
+})
 
 
 
