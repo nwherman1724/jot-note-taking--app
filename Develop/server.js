8014852d7b115cc1a7ae64a297e83dd91e53ae4e
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-02-12 21:18:57 -0500

    added wildcard route

--- Develop/server.js
+++ Develop/server.js
@@ -9,8 +9,14 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
+
+
 
 app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`))
