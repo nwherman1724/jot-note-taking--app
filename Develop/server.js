16cc48160faaf430f0767f8fa894adec2fa4668e
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-02-15 07:30:48 -0500

    added post statement

--- Develop/server.js
+++ Develop/server.js
@@ -27,36 +27,62 @@ app.get('/notes', (req, res) => {
 
 // get request that return notes from db
 app.get('/api/notes', (req,res) => {
-    res.json(notes);
+    fs.readFile('./db/db.json', 'utf8', (err,data) => {
+      if(err){
+        console.error(err)
+      } else  
+    
+    res.json(JSON.parse(data));
+  })
 })
 
-// app.post('/notes', (req, res) => {
-//   // Log that a POST request was received
-//   console.info(`${req.method} request received to add a review`);
 
-//   // Destructuring assignment for the items in req.body
-//   const { title, text } = req.body;
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a review`);
 
-//   // If all the required properties are present
-//   if (title && text) {
-//     // Variable for the object we will save
-//     const newNote = {
-//       title,
-//       text,
-//       note_id: uuid(),
-//     };
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
 
-//     const response = {
-//       status: 'success',
-//       body: newNote,
-//     };
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
 
-//     console.log(response);
-//     res.status(201).json(response);
-//   } else {
-//     res.status(500).json('Error in posting review');
-//   }
-// });
+    fs.readFile('./db/db.json', 'utf8', (err,data) => {
+      if(err){
+        console.error(err)
+      } else {
+        const parsedNotes = JSON.parse(data)
+        parsedNotes.push(newNote)
+
+        const noteStr = JSON.stringify(parsedNotes, null, 2);
+
+        fs.writeFile('./db/db.json', noteStr, (err) => {
+        if(err){
+          console.log(err)
+        }else {
+          console.log(`${newNote.title} has been writtne to the JSON file!`)
+        }
+       })
+      }
+    })
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting review');
+  }
+});
 
 // get request/ wild card route that return index.html
 app.get('*', (req, res) =>
